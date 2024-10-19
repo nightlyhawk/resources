f76801f6cb6737ea8a20e9cf1f71ebdc137517b8
nightlyhawk <georgehawk30@gmail.com>
2024-10-19 13:05:35 +0100

    pageManifest

--- src/layout/navbar/index.js
+++ src/layout/navbar/index.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import { NavBtn, NavContainer, NavIcon, NavItem, NavWrapper } from './style'
+import React, { useState } from 'react'
+import { NavBtn, NavContainer, NavIcon, NavItem, NavSub, NavWrapper } from './style'
 import logo from "../../assets/icons/logo.svg"
 import arrowdown from "../../assets/icons/arrowdown.svg"
+import burger from "../../assets/icons/burger.svg"
 
 
 const NavBar = () => {
+    const [see, setSee] = useState(false);
+
   return (
     <NavWrapper>
         <NavItem to={"/"}>
             <NavIcon $ht="40px"  $wt="136px" src={logo} />
         </NavItem>
-        <NavContainer>
-            <NavItem to={""}>Products <NavIcon src={arrowdown} /></NavItem>
-            <NavItem to={""}>About us <NavIcon src={arrowdown} /></NavItem>
-            <NavItem to={""}>Contact us <NavIcon src={arrowdown} /></NavItem>
-            <NavItem to={""}>Pricing <NavIcon src={arrowdown} /></NavItem>
-        </NavContainer>
-        <NavBtn>Request Demo</NavBtn>
+        <NavSub $seen={see}>
+            <NavContainer>
+                <NavItem to={""}>Products <NavIcon src={arrowdown} /></NavItem>
+                <NavItem to={""}>About us <NavIcon src={arrowdown} /></NavItem>
+                <NavItem to={""}>Contact us <NavIcon src={arrowdown} /></NavItem>
+                <NavItem to={""}>Pricing <NavIcon src={arrowdown} /></NavItem>
+            </NavContainer>
+            <NavBtn>Request Demo</NavBtn>
+        </NavSub>
+        <NavIcon src={burger} alt='menu' $control={"none"} onClick={() => setSee(!see)}/>
     </NavWrapper>
   )
 }
--- src/layout/navbar/style.js
+++ src/layout/navbar/style.js
@@ -6,12 +6,29 @@ export const NavWrapper = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 1%;
+    position: relative;
 `
+export const NavSub = styled.div`
+    display: flex;
+    width: 70%;
+    justify-content: space-between;
 
+    @media screen and (max-width: 800px){
+        display: ${props => props.$see? "flex" : "none"};
+        flex-direction: column;
+        width: 40%;
+        gap: 3rem;
+        border: none;
+        border-radius: 10px;
+        padding: 1%;
+        position: absolute;
+        box-shadow: 1px 0 1px slategray;
+    }
+`
 export const  NavContainer = styled.nav`
     display: flex;
     justify-content: space-between;
-    width: 42%;
+    width: 54%;
 `
 
 export const NavItem = styled(Link)`
@@ -26,6 +43,11 @@ export const NavItemText =  styled.p``
 export const NavIcon = styled.img`
     height: ${props => props.$ht};
     width: ${props => props.$wt};
+    display: ${props => props.$control || "block"};
+    @media screen and (max-width: 800px){
+        display: block;
+    }
+
 `
 
 export const NavBtn = styled.button`
